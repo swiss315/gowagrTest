0a62f9d9c909b55c23f656099793f52ea75d6290
swiss315 <habeeb.sunmonud@gmail.com>
2025-05-30 14:56:07 +0100

    habeeb update

--- src/services/api/playeronTarget.ts
+++ src/services/api/playeronTarget.ts
@@ -2,7 +2,7 @@ import {PlayerShotsOnTargetResponse} from "@/services/api/constant/type";
 
 export async function fetchPlayerShotsOnTarget(size: number = 30, page: number = 1): Promise<PlayerShotsOnTargetResponse> {
     try {
-        const url = `https://api.squads.game/bet/public-props?marketType=player_shots_on_target&size=${size}&page=${page}`;
+        const url = `https://staging-api.squads.game/bet/public-props?marketType=player_shots_on_target&size=${size}&page=${page}`;
 
         const response = await fetch(url, {
             method: 'GET',
